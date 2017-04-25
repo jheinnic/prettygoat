a4ac9aa54317a75e29f917d2f30bafe7cf1224ed
John Heinnickel <john.heinnickel@bstocksolutions.com>
2017-04-24 18:05:18 -0700

    Less "manual" Route decorator

--- scripts/web/RouteDecorator.ts
+++ scripts/web/RouteDecorator.ts
@@ -1,14 +1,31 @@
 import {decorate, injectable} from "inversify";
 import Methods from "./Methods";
+import {makeDecorator} from "reflect-helper";
+
+export class RouteDecorator {
+    method: Methods;
+    path: string;
+}
+
+const applyRoute = makeDecorator(RouteDecorator);
+// const applyRoute = <MethodDecorator|ClassDecorator> makeDecorator(RouteDecorator);
+
+// function Route(method: Methods, path: string) {
+//     return function (target: any) {
+//         decorate(injectable(), target);
+//         Reflect.defineMetadata("prettygoat:method", method, target);
+//         Reflect.defineMetadata("prettygoat:path", path, target);
+//         return target;
+//     };
+// }
 
 function Route(method: Methods, path: string) {
-    return function (target: any) {
+    const decoFn = applyRoute(method, path);
+    return function (target: any, ...args: any[]) {
         decorate(injectable(), target);
-        Reflect.defineMetadata("prettygoat:method", method, target);
-        Reflect.defineMetadata("prettygoat:path", path, target);
-        return target;
-    };
-}
+        decoFn.apply(target, args);
+    }
+};
 
 export default Route
 
